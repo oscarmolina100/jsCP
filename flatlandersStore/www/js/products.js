c5819185f47be024e47eed34d2823d003c3f8c9d
Jesús Oscar Molina Alvarado <oscar_molina100@msn.com>
2017-06-16 12:20:19 -0600

    Final de cambios

--- flatlandersStore/www/js/products.js
+++ flatlandersStore/www/js/products.js
@@ -44,4 +44,18 @@
         };
     });
 
+    app.directive('productGallery', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'product-gallery.html',
+            controller: function(){
+                this.current = 0;
+                this.setCurrent = function(imageNumber){
+                    this.current = imageNumber || 0;
+                };
+            },
+            controllerAs: 'g'
+        };
+    });
+
 })();
