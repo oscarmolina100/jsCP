c8dfb55eac779ff30a33129b0df9cf1a112f672f
Jesús Oscar Molina Alvarado <oscar_molina100@msn.com>
2017-06-16 10:51:03 -0600

    Cambio de directivas

--- flatlandersStore/www/js/app.js
+++ flatlandersStore/www/js/app.js
@@ -1,6 +1,6 @@
 (function(){
 
-    let app = angular.module('store', []);
+    let app = angular.module('store', ['store-directives']);
 
     app.controller('StoreController', ['$http', function($http){
         let s = this;
@@ -11,47 +11,5 @@
             s.products = data.data;
         })
     }]);
-
-    app.directive('productTitle', function(){
-        return {
-            restrict: 'E',
-            templateUrl: 'product-title.html'
-        };
-    });
-
-    app.directive('productPanels', function(){
-        return{
-            restrict: 'E',
-            templateUrl: 'product-panels.html',
-            controller: function(){
-                this.tab = 1;
-
-                this.selectTab = function(setTab){
-                    this.tab = setTab; 
-                }
-
-                this.isSelected = function(checkTab){
-                    return this.tab === checkTab;
-                }
-            },
-            controllerAs: 'p'
-        };
-    });
-
-    app.directive('productReviews', function(){
-        return{
-            restrict: 'E',
-            templateUrl: 'product-reviews.html',
-            controller: function(){
-                this.review = {};
-
-                this.addReview = function(product){
-                    product.reviews.push(this.review);
-                    this.review = {};
-                }
-            },
-            controllerAs: 'r'
-        }
-    })
 
 })();
